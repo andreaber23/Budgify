988b829c07e7090f12cd2173844e53f56baeaabe
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-07-12 21:00:26 -0400

    user root updated, index.js. Added a utils forlder for the validation

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRouter = require('./homeRouter');
 
-router.use('/', homeRoutes);
+router.use('/', homeRouter);
 router.use('/api', apiRoutes);
 
 module.exports = router;
