06824d1dfe4b60b167d496d4cd55f61306b0d84d
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-07-13 21:20:35 -0400

    expenseData.json array created, still needs more. seed.js completed

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Expense } = require('../model');
+
+const userData = require('./userData.json');
+const expenseData = require('./expenseData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+      });
+    
+      for (const expense of expenseData) {
+        const randomUser = users[Math.floor(Math.random() * users.length)];
+        await Expense.create({
+            ...expense,
+            user_id: randomUser.id,
+        
+        });
+      }
+     process.exit(0);
+}
+seedDatabase();
