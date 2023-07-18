c4a2daa5c4901a6117c926b82e81f7da4a8cac3c
Zach Wellens <zachwellens990@gmail.com>
2023-07-18 15:34:39 -0400

    Wrote helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,35 @@
 module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
-    format_amount: (amount) => {
-      // format large numbers with commas
-      return parseInt(amount).toLocaleString();
-    },
-    get_emoji: () => {
-      const randomNum = Math.random();
-  
-      // Return a random emoji
-      if (randomNum > 0.7) {
-        return `<span for="img" aria-label="lightbulb">💡</span>`;
-      } else if (randomNum > 0.4) {
-        return `<span for="img" aria-label="laptop">💻</span>`;
-      } else {
-        return `<span for="img" aria-label="gear">⚙️</span>`;
+    get_monthly_expenses: (userData) => {
+      const userExpenses = Expense.findAll({
+        where: {user_id: userData.id},
+        include: {attributes: ['amount', 'paymentSchedule']}
+      });
+      
+      var totalMonthlyExpenses = 0;
+      for (let i = 0; i < userExpenses.length; i++) {
+        if (userExpenses[i].paymentSchedule == "monthly") {
+          totalMonthlyExpenses += userExpenses[i].amount;
+        }
       }
+
+      return parseInt(totalMonthlyExpenses).toLocaleString();
+    },
+    get_annual_expenses: (userData) => {
+      const userExpenses = Expense.findAll({
+        where: {user_id: userData.id},
+        include: {attributes: ['amount', 'paymentSchedule']}
+      });
+      
+      var totalAnnualExpenses = 0;
+      for (let i = 0; i < userExpenses.length; i++) {
+        if (userExpenses[i].paymentSchedule == "monthly") {
+          totalAnnualExpenses += (userExpenses[i].amount * 12);
+        } else {
+          totalAnnualExpenses += (userExpenses[i].amount);
+        }
+      }
+
+      return parseInt(totalAnnualExpenses).toLocaleString();
     },
   };
   
